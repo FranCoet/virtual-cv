1361c4da226d6123882ef0792fdc641dd62cd49c
FranCoet <168994153+FranCoet@users.noreply.github.com>
2026-07-24 14:42:03 +0200

    added automatic footer year

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const testButton = document.querySelector("#test-button")
-const testMessage = document.querySelector("#test-message");
-
-
-
-testButton.addEventListener("click", () => {
-    testMessage.textContent = 
-    "Success! The files are connected"
-});
+//this automatically will update the footer year.
+const currentYear = document.querySelector("#current-year");
+currentYear.textContent  = new Date().getFullYear();
